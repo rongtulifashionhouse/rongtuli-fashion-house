bbb73ef252503f0789bd91b0e39341632ea06a3a
Rongtuli Fashion House <rongtulifashionhouse11@gmail.com>
2026-07-26 12:45:21 +0600

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,1 +1,22 @@
+// ===== Admin Login =====
 
+function loginAdmin(){
+
+    const username =
+    document.getElementById("username").value;
+
+    const password =
+    document.getElementById("password").value;
+
+    if(username==="admin" && password==="123456"){
+
+        alert("লগইন সফল!");
+
+    }else{
+
+        document.getElementById("loginMessage")
+        .innerText="ইউজারনেম অথবা পাসওয়ার্ড ভুল হয়েছে।";
+
+    }
+
+}
